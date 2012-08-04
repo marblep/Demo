6cf5b8012d19df88b0a1de255c5a8029ed2aa176
marblep <marblep@hotmail.com>
2012-08-05 01:21:12 +0800

    parse json

--- LitJsonDemo/LitJsonDemo/MainWindow.xaml.cs
+++ LitJsonDemo/LitJsonDemo/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using LitJson;
 
 namespace LitJsonDemo
 {
@@ -19,9 +21,53 @@ namespace LitJsonDemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string content_;
+
+        public enum AwardType
+        {
+            Kill=0, Assist, DoubleKill, TripleKill, GetSelfFlag,
+        }
+        private Dictionary<AwardType, Dictionary<string, int>> DropDictionary_ = new Dictionary<AwardType, Dictionary<string, int>>();
+        private TeamDeathType teamdeath_ = new TeamDeathType();
+        
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            OpenJsonFile();
+            ParseJsonFile();
+        }
+
+        private void ParseJsonFile()
+        {
+            JsonData jsondata = JsonMapper.ToObject(content_);
+
+            var lines = jsondata["DropConfig"];
+            for (int i = 0; i < lines.Count; i++)
+            { 
+                var line = lines[i];
+
+            }
+        }
+
+        private void OpenJsonFile()
+        { 
+            string path = System.IO.Path.Combine(System.Environment.CurrentDirectory, @"..\..\DropConfig.txt");
+            if(File.Exists(path))
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    content_ = sr.ReadToEnd();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Can't find the file");
+            }
         }
     }
 }
