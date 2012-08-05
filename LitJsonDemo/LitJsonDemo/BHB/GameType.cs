574ebf23a4a382853db288c501ba8ae0478e5aaa
marblep <marblep@hotmail.com>
2012-08-05 16:38:07 +0800

    Finish

--- LitJsonDemo/LitJsonDemo/BHB/GameType.cs
+++ LitJsonDemo/LitJsonDemo/BHB/GameType.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 
 
-class GameType
+public class GameType
 {
 
 
--- LitJsonDemo/LitJsonDemo/MainWindow.xaml.cs
+++ LitJsonDemo/LitJsonDemo/MainWindow.xaml.cs
@@ -28,18 +28,45 @@ namespace LitJsonDemo
             Kill=0, Assist, DoubleKill, TripleKill, GetSelfFlag,
         }
         private Dictionary<AwardType, Dictionary<string, int>> DropDictionary_ = new Dictionary<AwardType, Dictionary<string, int>>();
-        private TeamDeathType teamdeath_ = new TeamDeathType();
-        
+        private int FlagXP = 0x00000001;
+        private int FlagTicket = 0x00000010;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        public bool IsDropXP(AwardType _awardtype, GameType _gametype)
+        {
+            return ((DropDictionary_[_awardtype][_gametype.GetType().ToString()] & FlagXP) != 0);
+        }
+
+        public bool IsDropTicket(AwardType _awardtype, GameType _gametype)
+        {
+            return ((DropDictionary_[_awardtype][_gametype.GetType().ToString()] & FlagTicket) != 0);
+        }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             OpenJsonFile();
             ParseJsonFile();
+            Test();
+        }
+
+        private void Test()
+        {
+            GameType[] gametypes = {new TeamDeathType(), new CaptureType(), new HotPotatoType()};
+            foreach (AwardType type in Enum.GetValues(typeof(AwardType)))
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine(" {0}, {1}, Ticket: {2}, XP:{3}", type.ToString(),
+                                                                       gametypes[i],
+                                                                       IsDropTicket(type, gametypes[i]),
+                                                                       IsDropXP(type, gametypes[i]));
+                }
+                    
+            }
         }
 
         private void ParseJsonFile()
@@ -50,7 +77,16 @@ namespace LitJsonDemo
             for (int i = 0; i < lines.Count; i++)
             { 
                 var line = lines[i];
+                string sevent = line["Event"].ToString();
+                AwardType type = (AwardType)Enum.Parse(typeof(AwardType), sevent);
 
+                Dictionary<string, int> dropInfo = new Dictionary<string, int>();
+                dropInfo.Add("TeamDeathType", GetFlag(line["TeamDeathType"].ToString() ));
+                dropInfo.Add("CaptureType", GetFlag(line["CaptureType"].ToString()));
+                dropInfo.Add("HotPotatoType", GetFlag(line["HotPotatoType"].ToString()));
+
+                DropDictionary_.Add(type, dropInfo);
+                //Console.WriteLine("--- {0} ---", sdrop);
             }
         }
 
@@ -68,6 +104,17 @@ namespace LitJsonDemo
             {
                 MessageBox.Show("Can't find the file");
             }
+        }
+
+        private int GetFlag(string _rawflags)
+        {
+            string[] flags = _rawflags.Split(new char[] { '&' });
+            int iFlag = 0;
+            if (flags.Contains("T"))
+                iFlag |= FlagTicket;
+            if (flags.Contains("X"))
+                iFlag |= FlagXP;
+            return iFlag;
         }
     }
 }
